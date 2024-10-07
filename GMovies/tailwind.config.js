b83aef34bb3cde737bceb44f1e4dcb75c7634b5f
anagiovanni <anagiovanni66@gmail.com>
2024-10-07 12:23:26 -0300

    criado o projeto e suas dependências

---
+++ GMovies/tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: [
+    "./App.{js,jsx,ts,txs}",
+    "./screens/**/*.{js,jsx,ts,txs}",
+    "./components/**/*.{js,jsx,ts,txs}"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
